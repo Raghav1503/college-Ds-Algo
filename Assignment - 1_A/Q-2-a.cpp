c6ed0e087b3ebc96fe1537c33d04f35b1ffc319f
Raghav Prabhakar <raghavprabhakar@pop-os.localdomain>
2020-08-10 14:41:16 +0530

    improved storing method and printing transpose matrix

--- Assignment - 1_A/Q-2-a.cpp
+++ Assignment - 1_A/Q-2-a.cpp
@@ -11,44 +11,43 @@ int main()
     int matrix[rows][cols];
     cout<<"Enter the elements of the Matrix : "<<endl;
     
+    int sparse_matrix[100][3], k=0;
     for(int i=0; i<rows;i++)
     {
         cout<<"Enter Row "<<i+1<<endl;
         for(int j=0; j<cols; j++)
         {   
             cin>>n;
-            matrix[i][j] = n;
+            if(n != 0)
+        {   
+            sparse_matrix[k][0] = i;
+            sparse_matrix[k][1] = j;
+            sparse_matrix[k][2] = n;
+            k++;
         }
-        cout<<endl;
+        }
     }
     cout<<endl;
 
-    cout<<"MATRIX = "<<endl;
+    cout<<"FINAL MATRIX\n";
+    int l=0;
     for(int i=0; i<rows;i++)
     {
         for(int j=0; j<cols; j++)
         {
-            cout<<matrix[i][j]<<" ";
+            if(sparse_matrix[l][0] == i && sparse_matrix[l][1] == j)
+            {
+                cout<<sparse_matrix[l][2]<<" ";
+                l++;
+            }
+            else
+            {
+                cout<<0<<" ";
+            }
         }
         cout<<endl;
     }
     cout<<endl;
-
-    int sparse_matrix[100][3], k=0;
-    for(int i=0; i<rows;i++)
-    {   
-        for(int j=0; j<cols; j++)
-        {
-            if(matrix[i][j] != 0)
-        {   
-            sparse_matrix[k][0] = i;
-            sparse_matrix[k][1] = j;
-            sparse_matrix[k][2] = matrix[i][j];
-            k++;
-        }
-        }
-        
-    }
     
     for(int i=0; i<k;i++)
     {
@@ -75,16 +74,36 @@ int main()
     }
     cout<<endl;
 
-    // Converting back to matrix
-    /*int l=0;
+    // SORTING
+    for(int j=0; j<k-1; j++)
+    {
+        for(int i=0;i<k-j-1;i++)
+        {
+            if(sparse_matrix[i][0] > sparse_matrix[i+1][0])
+            {
+                int temp1 = sparse_matrix[i][0], temp2 = sparse_matrix[i][1], temp3 = sparse_matrix[i][2];
+                
+                sparse_matrix[i][0] = sparse_matrix[i+1][0];
+                sparse_matrix[i][1] = sparse_matrix[i+1][1];
+                sparse_matrix[i][2] = sparse_matrix[i+1][2];
+            
+                sparse_matrix[i+1][0] = temp1;
+                sparse_matrix[i+1][1] = temp2;
+                sparse_matrix[i+1][2] = temp3;
+            }
+        }
+    }
+
+    cout<<"FINAL MATRIX\n";
+    int a=0;
     for(int i=0; i<rows;i++)
     {
         for(int j=0; j<cols; j++)
         {
-            if(sparse_matrix[l][0] == i && sparse_matrix[l][1] == j)
+            if(sparse_matrix[a][0] == i && sparse_matrix[a][1] == j)
             {
-                cout<<sparse_matrix[l][2]<<" ";
-                l++;
+                cout<<sparse_matrix[a][2]<<" ";
+                a++;
             }
             else
             {
@@ -94,6 +113,6 @@ int main()
         cout<<endl;
     }
     cout<<endl;
-*/
+
     return 0;
 }
