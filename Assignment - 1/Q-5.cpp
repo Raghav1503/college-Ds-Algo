c5d1f4596b9b00affd90f6e275520fd58d33a738
Raghav Prabhakar <raghavprabhakar@pop-os.localdomain>
2020-08-10 16:12:05 +0530

    fixed Q-5 and Q-6 of A1

--- Assignment - 1/Q-5.cpp
+++ Assignment - 1/Q-5.cpp
@@ -10,9 +10,51 @@ Search.
 #include <iostream>
 using namespace std;
 
+void sort(int*, int);
+int binary_search(int*, int, int, int);
+
+void sort(int arr[], int len)
+{
+    // #### BUBBLE SORT #####
+    for(int i=0; i<len; i++)
+    {
+        for(int j=0; j<len-i-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }        
+    }
+}
+
+int binary_search(int arr[], int start, int end, int search)
+{
+    int mid = (start+end)/2;
+
+    if (arr[mid] == search)
+    {
+        return mid;
+    }
+    else if (arr[mid] < search)
+    {
+        binary_search(arr, mid+1, end, search);
+    }
+    else if (arr[mid] > search)
+    {
+        binary_search(arr, start, mid-1, search);
+    }
+
+    return -1;
+
+}
+
+
 int main()
 {
-    int len, n;
+    int len=0, n=0, search=0;
     
     cout<<"Please Enter the Length of the Array : ";
     cin>>len;
@@ -25,41 +67,23 @@ int main()
         arr[i] = n;
     }
 
-    cout<<"UNSORTED ARRAY : "<<endl;
-    for (int i=0; i<len; i++)
+    cout<<"Please enter the number you want to search : ";
+    cin>>search;
+
+    sort(arr, len);
+
+    int start = 0, end=len;
+    int pos = binary_search(arr, start, end, search);
+
+    if (pos > 0)
     {
-        cout<<arr[i]<<"\t";
+        cout<<"Found Number "<<search<<" at postion "<<pos+1<<" .\n";
     }
-    cout<<endl<<endl;
+    else
+    {
+        cout<<"ERROR 404! NOT FOUND"<<endl;
+    }
     
-   // #### BUBBLE SORT #####
-    for(int i=0; i<len; i++)
-    {
-
-        for(int j=0; j<len-i-1; j++)
-        {
-            if(arr[j] > arr[j+1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-            for(int i=0; i<len; i++)
-            {
-                cout<<arr[i]<<"\t";
-            }
-            cout<<endl;
-        }
-        
-    }
-
-    std::cout<<"\nSORTED ARRAY : "<<endl;
-    for(int i=0; i<len; i++)
-    {
-        cout<<arr[i]<<"\t";
-    }
-    cout<<endl;
-
-
-    return 0;
+   
+   return 0;
 }
--- Assignment - 1/Q-6.cpp
+++ Assignment - 1/Q-6.cpp
@@ -5,56 +5,12 @@ swapping the adjacent elements if they are in wrong order. Code the
 Bubble sort with the following elements:
 
 */
-
-
 #include <iostream>
 using namespace std;
 
-void sort(int*, int);
-int binary_search(int*, int, int, int);
-
-void sort(int arr[], int len)
-{
-    // #### BUBBLE SORT #####
-    for(int i=0; i<len; i++)
-    {
-        for(int j=0; j<len-i-1; j++)
-        {
-            if(arr[j] > arr[j+1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }        
-    }
-}
-
-int binary_search(int arr[], int start, int end, int search)
-{
-    int mid = (start+end)/2;
-
-    if (arr[mid] == search)
-    {
-        return mid;
-    }
-    else if (arr[mid] < search)
-    {
-        binary_search(arr, mid+1, end, search);
-    }
-    else if (arr[mid] > search)
-    {
-        binary_search(arr, start, mid-1, search);
-    }
-
-    return -1;
-
-}
-
-
 int main()
 {
-    int len=0, n=0, search=0;
+    int len, n;
     
     cout<<"Please Enter the Length of the Array : ";
     cin>>len;
@@ -67,23 +23,43 @@ int main()
         arr[i] = n;
     }
 
-    cout<<"Please enter the number you want to search : ";
-    cin>>search;
-
-    sort(arr, len);
-
-    int start = 0, end=len;
-    int pos = binary_search(arr, start, end, search);
-
-    if (pos > 0)
+    cout<<"UNSORTED ARRAY : "<<endl;
+    for (int i=0; i<len; i++)
     {
-        cout<<"Found Number "<<search<<" at postion "<<pos+1<<" .\n";
+        cout<<arr[i]<<"\t";
     }
-    else
-    {
-        cout<<"ERROR 404! NOT FOUND"<<endl;
-    }
+    cout<<endl<<endl;
     
-   
-   return 0;
+   // #### BUBBLE SORT #####
+    for(int i=0; i<len; i++)
+    {
+
+        for(int j=0; j<len-i-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+            for(int i=0; i<len; i++)
+            {
+                cout<<arr[i]<<"\t";
+            }
+            cout<<endl;
+        }
+        
+    }
+
+    std::cout<<"\nSORTED ARRAY : "<<endl;
+    for(int i=0; i<len; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<endl;
+
+
+    return 0;
 }
+
+
